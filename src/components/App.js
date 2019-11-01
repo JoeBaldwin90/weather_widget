f9328709070ad2d7a9d51276f64b7e798d320958
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-11-01 13:52:06 +0000

    Create propData object to store chosen API response data

--- src/components/App.js
+++ src/components/App.js
@@ -21,12 +21,33 @@ class App extends Component {
   getWeather = () => {
     const { lat, long } = this.state.userLatLong;
     const apiKey = "349a4fd78de1c4a4c581588ddfbf45ef";
+    
     const endPoint = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`;
 
     fetch(endPoint)
       .then(response => response.json())
       .then(apiData => {
-        console.log(apiData);
+
+        const temporalData = apiData.list;
+        const propData = {};
+
+        propData.location = apiData.city.name;
+        propData.weatherIntervals = temporalData.map(record => ({
+          date: record.dt,
+          icon: record.weather[0].icon,
+          description: record.weather[0].description,
+          temperature: record.main.temp,
+          pressure: record.main.pressure,
+          humidity: record.main.humidity,
+          cloud: record.clouds.all,
+          wind: {
+            speed: record.wind.speed,
+            direction: record.wind.deg
+          }
+        }));
+
+        console.log(propData);
+
         this.setState({
           loading: false
         });
