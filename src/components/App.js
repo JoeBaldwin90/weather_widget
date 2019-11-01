76a4c27fde9c2f9dbb2137efede1bf5f7c1d7fa6
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-11-01 14:14:24 +0000

    Use metric endpoint values to display temperature in Celcius

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,7 @@ class App extends Component {
     const { lat, long } = this.state.userLatLong;
     const apiKey = "349a4fd78de1c4a4c581588ddfbf45ef";
 
-    const endPoint = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`;
+    const endPoint = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
 
     fetch(endPoint)
       .then(response => response.json())
